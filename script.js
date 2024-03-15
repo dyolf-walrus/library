ebe56da1c83dde7ccb6fdd6fb366e4e3cfb082cd
Allison <124654013+dyolf-walrus@users.noreply.github.com>
2024-03-14 21:03:48 -0400

    Start css, set library html in script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: "The Giver",
+        author: "I have no idea",
+        pages: 567,
+        read: true
+    },
+    {
+        title: "The Dictionary",
+        author: "Deez  Nuts",
+        pages: 12,
+        read: false
+    }
+];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -16,3 +29,23 @@ function addBookToLibrary() {
     let newBook = new Book(title, author, pages, read)
     myLibrary.push(newBook)
 }
+
+function setLibraryHtml(myLibrary) {
+    let myHtml = '';
+    let readOrNot;
+    for (const book of myLibrary) {
+        if (book.read == true) {
+            readOrNot = `<span class="read-true">Already read this book</span>`
+        } else readOrNot = `<span class="read-false">Need to read this book</span>`
+        myHtml += `<div class="card">
+        <span class="book-title">${book.title}</span>
+        <span class="book-author">by ${book.author}</span>
+        <span class="page-number">${book.pages} pages long</span>
+        ${readOrNot}
+        </div>`
+    }
+    document.getElementById('library').setHTML(myHtml);
+    console.log(myHtml)
+}
+
+setLibraryHtml(myLibrary);
